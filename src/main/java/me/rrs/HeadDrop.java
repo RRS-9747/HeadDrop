9dcf57d16483cc20b4b4bd0e84b85be25230bd5e
RRS <39312279+RRS-9747@users.noreply.github.com>
2022-03-01 17:36:34 +0600

    Add files via upload

--- src/main/java/me/rrs/HeadDrop.java
+++ src/main/java/me/rrs/HeadDrop.java
@@ -3,9 +3,13 @@ package me.rrs;
 import me.rrs.Commands.*;
 import me.rrs.Listeners.*;
 import me.rrs.Util.Metrics;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import javax.security.auth.login.LoginException;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -15,12 +19,17 @@ import java.nio.file.Paths;
 public class HeadDrop extends JavaPlugin {
 
     private static HeadDrop instance;
+    private static JDA Bot;
 
 
     public static HeadDrop getInstance() {
         return instance;
     }
 
+
+    public static JDA getBot() {
+        return Bot;
+    }
 
     @Override
     public void onEnable() {
@@ -29,12 +38,16 @@ public class HeadDrop extends JavaPlugin {
         if (!this.getDescription().getName().equals("HeadDrop")){
             Bukkit.getLogger().severe("You can't change my name!");
             Bukkit.getPluginManager().disablePlugin(this);
+            return;
         }
 
         saveDefaultConfig();
 
 
         Metrics metrics = new Metrics(this, 13554);
+        String token = this.getConfig().getString("Bot.Token");
+
+
         getServer().getPluginManager().registerEvents(new PlayerDeath(), this);
         getServer().getPluginManager().registerEvents(new EntityDeath(), this);
         getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
@@ -45,6 +58,22 @@ public class HeadDrop extends JavaPlugin {
         getCommand("customhead").setExecutor(new CustomHead());
         getCommand("update").setExecutor(new AutoUpdate());
         getCommand("headdrop").setTabCompleter(new me.rrs.TabCompleter.HeadDrop());
+
+        if (this.getConfig().getBoolean("Bot.Enable")) {
+            try {
+                 Bot = JDABuilder.createDefault(token)
+                         .setActivity(Activity.watching("For Head"))
+                         .build();
+
+                 String name = Bot.getSelfUser().getName();
+                 String discriminator = Bot.getSelfUser().getDiscriminator();
+                 Bukkit.getLogger().warning(name + discriminator + "Enable successfully!");
+
+            } catch (LoginException e) {
+                Bukkit.getLogger().severe("Error enabling bot!");
+                e.printStackTrace();
+            }
+        }
 
 
 
@@ -62,7 +91,7 @@ public class HeadDrop extends JavaPlugin {
 
 
             if (config.exists()) {
-                if (version != 2.4) {
+                if (version != 2.9) {
 
                     if (oc.exists()) {
                         oc.delete();
@@ -77,9 +106,7 @@ public class HeadDrop extends JavaPlugin {
 
                 }
             }
-
         }
-
     }
 
 
